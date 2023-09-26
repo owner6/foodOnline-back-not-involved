220340444fb444ec079b4dd44ca0a56d9f4eda96
owner6 <owner1147@gmail.com>
2023-09-26 10:56:44 +0300

    install pg-hstore

--- db.js
+++ db.js
@@ -6,3 +6,10 @@ const sequelize = new Sequelize('postgresql://postgres:1oa9x7@localhost:5432/pos
 });
 
 module.exports = sequelize;
+
+try {
+  await sequelize.authenticate();
+  console.log('Connection has been established successfully.');
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
